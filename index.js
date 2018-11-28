7c93724968088aaf9a057b678a06e13f7202a066
Ezra Augustin <elidoom88@gmail.com>
2018-11-28 14:04:55 +0100

    js

---
+++ index.js
@@ -0,0 +1,17 @@
+let paraRectangle = function(largeur,hauteur) {
+
+return 2 * (largeur + hauteur);
+
+}
+
+paraRectangle(5,2);
+
+
+
+let laireRectangle = function(largeur,hauteur) {
+
+return  (largeur * hauteur);
+
+}
+
+laireRectangle(7,3);
